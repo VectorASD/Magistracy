24fae2fbdf3cdddefd475fc5f470e8ec3ed967b4
VectorASD <vectorasd@vk.com>
2025-10-02 00:03:22 +0700

    Добавлена поддержка матриц вида и проекции. Самый компактный вариант вычислений матрицы вида по числу АЛУ-операций. Матрицы теперь можно переименовывать вместо буквы 'M' перед ' = '

--- 1 семестр/Современные проблемы информатики/lab1.py
+++ 1 семестр/Современные проблемы информатики/lab1.py
@@ -3,7 +3,14 @@ from pprint import pprint
 from misc import float_eq, edge_enumerate
 from number_decorator import decorate_num, exists_decor
 from random import random
+from math import pi, tan, sin, cos
 
+pi_2   = pi / 2
+pi_180 = pi / 180
+
+def ctg(num):
+    # return 1 / tan(num)
+    return tan(pi_2 - num) # равнозначны
 
 
 class Qubit:
@@ -93,6 +100,7 @@ class Matrix:
         assert N > 0
         assert all(len(row) == N for row in mat)
         self.mat = mat
+        self.letter = "M"
 
     def __repr__(self):
         mat = self.mat
@@ -106,9 +114,9 @@ class Matrix:
         lens = tuple(max(map(len, col)) for col in zip(*mat))
         format = ", ".join("{:%s}" % x for x in lens).format
 
-        sep = "\n         " if div_mode else "\n    "
+        sep = "\n" + " " * (len(self.letter) + len(' = ') + len('1/√2 ') * div_mode)
         Str = sep.join(f'{"⎧⎪⎩("[e]}{format(*row)}{"⎫⎪⎭)"[e]}' for e, row in edge_enumerate(mat))
-        return f"M = {'1/√2 ' if div_mode else ''}{Str}"
+        return f"{self.letter} = {'1/√2 ' if div_mode else ''}{Str}"
 
     def __mul__(self, R):
         T = type(R)
@@ -123,7 +131,8 @@ class Matrix:
             return Matrix(*(tuple(e * R for e in row) for row in self.mat))
 
         assert len(self.mat) == len(vec)
-        return T(*(sum(a * b for a, b in zip(row, vec)) for row in self.mat))
+        gen = (sum(a * b for a, b in zip(row, vec)) for row in self.mat)
+        return tuple(gen) if T is tuple else T(*gen)
 
     def __invert__(self): # conjugate и транспонирование
         return Matrix(*(tuple(e.conjugate() for e in row) for row in zip(*self.mat)))
@@ -145,14 +154,71 @@ class Matrix:
         return (H @ ~H) == I
 
     @staticmethod
+    def perspective(fovy, aspect, near, far):
+        fovy = ctg(fovy / 2)
+        return Matrix(*(
+            (fovy / aspect, 0, 0, 0),
+            (0, fovy, 0, 0),
+            (0, 0, -(far + near) / (far - near), -2 * far * near / (far - near)),
+            (0, 0, -1, 0)
+        ))
+
+    @staticmethod
+    def view(x, y, z, yaw, pitch, roll):
+        yaw   *= pi_180
+        pitch *= pi_180
+        roll  *= pi_180
+        sY, cY = sin(yaw),   cos(yaw)
+        sP, cP = sin(pitch), cos(pitch)
+        sR, cR = sin(roll),  cos(roll)
+        r00, r01, r02 = cY*cP, cY*sP*sR - sY*cR, cY*sP*cR + sY*sR
+        r10, r11, r12 = sY*cP, sY*sP*sR + cY*cR, sY*sP*cR - cY*sR
+        r20, r21, r22 =   -sP, cP*sR,            cP*cR
+        right   =  r00,  r10,  r20
+        up      =  r01,  r11,  r21
+        forward = -r02, -r12, -r22
+        x, y, z = -x, -y, -z
+        return Matrix(
+            (r00, r01, r02, r00*x + r01*y + r02*z),
+            (r10, r11, r12, r10*x + r11*y + r12*z),
+            (r20, r21, r22, r20*x + r21*y + r22*z),
+            (0,   0,   0,   1),
+        ), forward, right, up
+
+    def project(self, x, y, z):
+        x, y, z, w = self * (x, y, z, 1)
+        return x / w, y / w, z / w
+        # -0.01 -> -1 (близко)
+        # -100  ->  1 (далеко)
+
+    def __call__(self, letter):
+        self.letter = letter
+        return self
+
+    @staticmethod
     def test():
         print("\n~~~ test Matrix ~~~\n")
-        print(H)
+        print(H("H"))
         print(NOT * Q_0)
         print(H   * Q_0)
-        print(~H)
+        print((~H)("~H"))
         print(H.is_unitary())
         print(~C == C, C.is_unitary())
+        print()
+
+        proj = Matrix.perspective(90 * pi_180, 1, 0.01, 100)
+        print(Matrix.view(0, 0, 0, 60,  0,  0)[0]("Y-rot")) # sY = √3/2; cY = 1/2
+        print(Matrix.view(0, 0, 0,  0, 60,  0)[0]("P-rot")) # sP = √3/2; cP = 1/2
+        print(Matrix.view(0, 0, 0,  0,  0, 60)[0]("R-rot")) # sR = √3/2; cR = 1/2
+        view, F, R, U = Matrix.view(0, 0, 30, 0, 0, 0)
+        proj_view = proj @ view
+        print(proj("proj"))
+        print(view("view"))
+        print(proj_view("p@v"))
+        print("Forward:", F)
+        print("Right:",   R)
+        print("Up:",      U)
+        print(proj_view.project(5, 5/2, 0))
 
 I   = Matrix((1, 0), (0, 1))
 NOT = Matrix((0, 1), (1, 0))
