d3920376cee5015c7cc75e273d1b646ec7aec685
VectorASD <vectorasd@vk.com>
2025-11-16 18:45:33 +0700

    Теперь моё форматирование таблиц выравнивает числовые колонки справа

--- 1 семестр/Методы сбора и обработки данных в сети Интернет/utils.py
+++ 1 семестр/Методы сбора и обработки данных в сети Интернет/utils.py
@@ -4,14 +4,29 @@ def print_table(table: tuple[tuple, ...], stream, middle_sep = True):
     table: кортеж из кортежей, где первый кортеж — заголовки.
     """
     # вычисляем ширину каждой колонки
-    col_widths = [max(len(str(row[i])) for row in table) for i in range(len(table[0]))]
+    col_widths = tuple(max(len(str(row[i])) for row in table) for i in range(len(table[0])))
 
     line_separator = "".join(("+", *("-" * (w + 2) + "+" for w in col_widths), "\n"))
 
+    # определяем, числовая ли колонка
+    def skip_header():
+        it = iter(table)
+        next(it)
+        return it
+    types = (int, float, complex, type(None))
+    numeric_cols = tuple(
+        all(isinstance(v, types) for v in (row[i] for row in skip_header()))
+        for i in range(len(table[0]))
+    )
+
     # печать таблицы
     stream.write(line_separator)
-    for idx, row in enumerate(table):
-        line = "".join(("|", *(" " + str(cell).ljust(col_widths[i]) + " |" for i, cell in enumerate(row)), "\n"))
+    it = iter(table)
+    header = "".join(("|", *(" " + str(cell).ljust(width) + " |" for cell, width in zip(next(it), col_widths)), "\n"))
+    stream.write(header)
+    stream.write(line_separator)
+    for row in it:
+        line = "".join(("|", *(" " + (str(cell).rjust if is_num else str(cell).ljust)(width) + " |" for cell, width, is_num in zip(row, col_widths, numeric_cols)), "\n"))
         stream.write(line)
-        if middle_sep or not idx: stream.write(line_separator)
+        if middle_sep: stream.write(line_separator)
     if not middle_sep: stream.write(line_separator)
