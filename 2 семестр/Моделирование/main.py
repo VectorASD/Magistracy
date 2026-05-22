5e0041320b1fd3606f4da29b67ae907208121021
VectorASD <vectorasd@vk.com>
2026-05-22 15:58:47 +0700

    Перенёс всё питоновское в директорию sim_core, чтобы это заработало, как пакет. 'main.py' за пакетов подтверждает все 3 метода импортирования функции test_scheme_v2 из blocks.py

---
+++ 2 семестр/Моделирование/main.py
@@ -0,0 +1,10 @@
+import sim_core
+print(dir(sim_core))
+
+# sim_core.blocks.test_scheme_v2()
+
+# from sim_core import blocks
+# blocks.test_scheme_v2()
+
+from sim_core.blocks import test_scheme_v2
+test_scheme_v2()
